5d41076b6089ebe3c08e525a9649a96f447b8048
yoonisali <yoonismahamoudali@gmail.com>
2022-10-21 15:47:54 -0700

    hide the form and show the results

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,30 @@ function handleQuestion(event) {
   let python = document.querySelector("div#python");
   let script = document.querySelector("div#javascript");
   let ruby = document.querySelector("div#ruby");
+  let form = document.querySelector("form#program");
 
   const idealSpace = document.querySelector("input[name='idealSpace']:checked").value;
   const personType = document.querySelector("input[name='personType']:checked").value;
 
-  python.setAttribute('class', 'hidden');
-  script.setAttribute('class', 'hidden');
-  ruby.setAttribute('class', 'hidden');
-
+ 
   if (idealSpace === 'office' && personType === 'royal') {
     ruby.removeAttribute('class', 'hidden');
+    form.setAttribute('class', 'hidden');
   } else if (idealSpace === 'vacation' && personType === 'royal') {
     ruby.removeAttribute('class', 'hidden');
+    form.setAttribute('class', 'hidden');
   } else if (idealSpace === 'office' && personType === 'average') {
     script.removeAttribute('class', 'hidden');
+    form.setAttribute('class', 'hidden');
   } else if (idealSpace === 'vacation' && personType === 'average') {
     script.removeAttribute('class', 'hidden');
+    form.setAttribute('class', 'hidden');
   } else if (idealSpace === 'home' && personType === 'royal') {
     python.removeAttribute('class', 'hidden');
+    form.setAttribute('class', 'hidden');
   } else if (idealSpace === 'home' && personType === 'average') {
     python.removeAttribute('class', 'hidden');
+    form.setAttribute('class', 'hidden');
   }
 
 
